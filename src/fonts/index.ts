33ca3633d3ecca2efc9d726c634c9d565bbcbe86
th3bossc <diljith2003@gmail.com>
2025-01-15 18:56:12 +0530

    added colour and font configs

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,5 +1,5 @@
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Gloria_Hallelujah, Lato, Alexandria, Zen_Tokyo_Zoo } from "next/font/google";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -11,7 +11,37 @@ const geistMono = Geist_Mono({
     subsets: ["latin"],
 });
 
+
+const gloriaHallelujah = Gloria_Hallelujah({
+    subsets: ['latin'],
+    weight: ['400'],
+    variable: "--font-gloria-hallelujah",
+})
+
+const lato = Lato({
+    subsets: ['latin'],
+    weight: ['100', '300', '400', '700', '900'],
+    variable: "--font-lato",
+})
+
+const alexandria = Alexandria({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    variable: "--font-alexandria",
+})
+
+const zenTokyoZoo = Zen_Tokyo_Zoo({
+    subsets: ['latin'],
+    weight: ['400'],
+    variable: "--font-zen-tokyo-zoo",
+})
+
+
 export {
     geistMono,
     geistSans,
+    gloriaHallelujah,
+    lato,
+    alexandria,
+    zenTokyoZoo,
 }
