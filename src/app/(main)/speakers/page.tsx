78f51bc40c247b7bcb9abee7a82c0dadfd2e6d3d
th3bossc <diljith2003@gmail.com>
2025-01-29 18:18:48 +0530

    added basic page layout

---
+++ src/app/(main)/speakers/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/components/Container";
+import Section from "@/components/Section";
+
+const Page = () => {
+    return (
+        <Container>
+            <Section className="flex items-center justify-center w-full h-full">
+                speakers page
+            </Section>
+        </Container>
+    )
+}
+
+export default Page;
