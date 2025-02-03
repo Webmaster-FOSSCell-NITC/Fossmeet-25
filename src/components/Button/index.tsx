5e212b4c8251dbe567c88b561c48a3f1bd6a8c54
th3bossc <diljith2003@gmail.com>
2025-02-03 20:45:29 +0530

    changed button icon and routed navbar logo to main page

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,6 +4,7 @@
 import { JSX, MouseEventHandler, ReactNode, useCallback, useMemo } from "react";
 import { motion } from 'framer-motion'
 import { lato } from "@/fonts";
+import Image from "next/image";
 
 /**
  * global button component for a consistent design across pages
@@ -86,9 +87,11 @@ const Button = ({
                     {children}
                 </div>
                 <span className="absolute h-full right-[12px] top-[50%] translate-y-[-50%] flex items-center justify-center ">
-                    <span className="text-base">
-                        &rarr;
-                    </span>
+                <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g id="right-arrow 1">
+                        <path id="Vector" d="M22.6547 24.5883C22.4367 24.3703 22.4438 24.0258 22.6547 23.7937L27.3234 18.5625H7.3125C7.00312 18.5625 6.75 18.3094 6.75 18C6.75 17.6906 7.00312 17.4375 7.3125 17.4375H27.3234L22.6617 12.2062C22.4578 11.9672 22.4367 11.6367 22.6547 11.4187C22.8727 11.2008 23.2523 11.1867 23.4563 11.4117C23.4563 11.4117 29.025 17.5289 29.0813 17.5992C29.1375 17.6695 29.25 17.7961 29.25 18C29.25 18.2039 29.1375 18.3445 29.0813 18.4008C29.025 18.457 23.4563 24.5883 23.4563 24.5883C23.3508 24.6937 23.2031 24.75 23.0555 24.75C22.9078 24.75 22.7672 24.6937 22.6547 24.5883Z" className={inverted ? 'fill-white': `fill-secondary`} />
+                    </g>
+                </svg>
                 </span>
             </a>
         </motion.button >
