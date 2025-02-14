8abae7727df1dfe4da24845aed63262b9ec156d4
Naila Fathima <mac@NailaMacbook.local>
2025-02-14 21:53:45 +0530

    added loading to workshops and speaker page

--- src/app/(main)/speakers/page.tsx
+++ src/app/(main)/speakers/page.tsx
@@ -8,17 +8,28 @@ import { getSpeakers } from "@/lib/speakers";
 import styles from "../styles.module.css";
 import { SpeakerDetails } from "@/types";
 import useDisplayWidth from "@/hooks/DisplayWidth";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Page = () => {
     const [speakers, setSpeakers] = useState<SpeakerDetails[]>([]);
     const [selectedSpeaker, setSelectedSpeaker] = useState<SpeakerDetails | null>(null);
     const screenWidth = useDisplayWidth();
     const [numCols, setNumCols] = useState<number>(1);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchSpeakers = async () => {
-            const data = await getSpeakers();
-            setSpeakers(data);
+            try{
+                const data = await getSpeakers();
+                setSpeakers(data);
+            }
+            catch(error){
+                console.log(error);
+            }
+            finally{
+                setLoading(false);
+            }
+            
         };
         fetchSpeakers();
     }, []);
@@ -45,7 +56,9 @@ const Page = () => {
                 </span>
             </div>
 
-            <Section className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 place-items-center w-full h-full">
+            {
+                    !loading ?
+                    <Section className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 place-items-center w-full h-full">
 
                 {speakers.map((speaker, index) => (
                     <div
@@ -72,6 +85,24 @@ const Page = () => {
                     </div>
                 ))}
             </Section>
+                        
+                        :
+                        
+                        <div className="w-full h-full flex gap-1 justify-center items-center mt-20">
+                            <h1 className="text-secondary text-2xl">Loading</h1>
+                            <div className="flex space-x-1">
+                                {[0, 1, 2].map((i) => (
+                                    <motion.span
+                                        key={i}
+                                        className={`w-1 h-1 bg-primary rounded-full`}
+                                        animate={{ y: [0, 8, 0] }}
+                                        transition={{ duration: 0.6, repeat: Infinity, delay: i * 0.2 }}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+
+                }
 
         </Container>
     );
--- src/app/(main)/workshops/page.tsx
+++ src/app/(main)/workshops/page.tsx
@@ -7,13 +7,24 @@ import { getWorkshops } from "@/lib/workshops";
 import styles from "../styles.module.css";
 import { WorkshopDetails } from "@/types";
 
+import { motion, useScroll, useTransform } from "framer-motion";
+
 const Page = () => {
     const [Workshops, setWorkshops] = useState<WorkshopDetails[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchWorkshops = async () => {
-            const data = await getWorkshops();
-            setWorkshops(data);
+            setLoading(true);
+            try {
+                const data = await getWorkshops();
+                setWorkshops(data);
+            } catch(error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+
         };
         fetchWorkshops();
     }, []);
@@ -27,24 +38,49 @@ const Page = () => {
                 </span>
             </div>
 
-            <Section className="mb-24 grid grid-cols-1 gap-y-12 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center w-full h-full ">
+           
+                {
+                    !loading ?
+                    <Section className="mb-24 grid grid-cols-1 gap-y-12 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center w-full h-full ">
 
-                {Workshops.map((workshop, index) => (
-                    <WorkshopCard
-                        key={index}
-                        title={workshop.title}
-                        logoUrl={workshop.logoUrl}
-                        date={workshop.date}
-                        description={workshop.description}
-                        duration={workshop.duration}
-                        speakerName={workshop.speakerName}
-                        speakerImageUrl={workshop.speakerImageUrl}
-                        link={workshop.link}
+                            {Workshops.map((workshop, index) => (
+                                <WorkshopCard
+                                    key={index}
+                                    title={workshop.title}
+                                    logoUrl={workshop.logoUrl}
+                                    date={workshop.date}
+                                    description={workshop.description}
+                                    duration={workshop.duration}
+                                    speakerName={workshop.speakerName}
+                                    speakerImageUrl={workshop.speakerImageUrl}
+                                    link={workshop.link}
 
-                    />
+                                />
 
-                ))}
-            </Section>
+                            ))}
+
+                    </Section>
+                        
+                        :
+                        
+                        <div className="w-full h-full flex gap-1 justify-center items-center">
+                            <h1 className="text-secondary text-2xl">Loading</h1>
+                            <div className="flex space-x-1">
+                                {[0, 1, 2].map((i) => (
+                                    <motion.span
+                                        key={i}
+                                        className={`w-1 h-1 bg-primary rounded-full`}
+                                        animate={{ y: [0, 8, 0] }}
+                                        transition={{ duration: 0.6, repeat: Infinity, delay: i * 0.2 }}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+
+                }
+
+
+            
 
         </Container>
     )
