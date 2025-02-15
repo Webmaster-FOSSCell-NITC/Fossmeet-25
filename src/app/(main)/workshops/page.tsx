90fd83dc7325f27c85f220972c7f513bdc660e0d
Naila Fathima <mac@NailaMacbook.local>
2025-02-15 23:01:22 +0530

    made small changes

--- src/app/(main)/workshops/page.tsx
+++ src/app/(main)/workshops/page.tsx
@@ -59,7 +59,7 @@ const Page = () => {
 
             {
                     !loading ?
-                    <Section className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 place-items-center w-full h-full">
+                    <Section className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center w-full h-full">
 
                 {workshops.map((workshop, index) => (
                     <div
--- src/components/Modals/WorkshopModal/index.tsx
+++ src/components/Modals/WorkshopModal/index.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { lato } from "@/fonts";
 import { WorkshopDetails } from "@/types";
-import Image from "next/image";
 import { MouseEventHandler, useCallback, useMemo, useState } from "react";
 import { IoMdCloseCircleOutline } from "react-icons/io";
 import { AnimatePresence, motion } from "framer-motion";
@@ -32,8 +31,8 @@ const Modal: React.FC<ModalProps> = ({
 
     const modelOrientation = useMemo(() =>
         orientation === 'right'
-            ? "static lg:absolute top-0 right-0 lg:translate-x-[100%] w-[250px] sm:w-[300px] md:w-1/2 lg:w-[450px] px-[16px] py-[6px] h-[450px] md:h-full bg-white z-[5] overflow-y-scroll"
-            : "static lg:absolute top-0 left-0 lg:translate-x-[-100%] w-[250px] sm:w-[300px] md:w-1/2 lg:w-[450px] px-[16px] py-[6px] h-[450px] md:h-full bg-white z-[5] overflow-y-scroll"
+            ? "static lg:absolute top-0 right-0 lg:translate-x-[100%] w-[250px] sm:w-[300px] md:w-1/2 lg:w-[450px] px-[16px] py-[6px] h-auto bg-white z-[5] overflow-y-scroll"
+            : "static lg:absolute top-0 left-0 lg:translate-x-[-100%] w-[250px] sm:w-[300px] md:w-1/2 lg:w-[450px] px-[16px] py-[6px] h-auto bg-white z-[5] overflow-y-scroll"
         , [orientation]);
 
     return (
@@ -44,18 +43,6 @@ const Modal: React.FC<ModalProps> = ({
                 animate={{ backdropFilter: 'blur(8px)' }}
                 transition={{ duration: 0.3 }}
             />
-            <div className="relative w-[250px] sm:w-[300px] md:w-1/2 lg:w-[250px] h-[250px] sm:h-[300px] md:h-1/2 lg:h-[400px] bg-primary shadow-lg flex-col justify-between overflow-hidden">
-                <div className="absolute left-0 bottom-0 w-[200px] lg:w-[300px] h-[234px] lg:h-[350px] flex flex-col justify-end">
-                    <Image
-                        src={workshop.logoUrl}
-                        alt={`${workshop.title}-picture`}
-                        className="object-fit-cover"
-                        priority
-                        width={500}
-                        height={500}
-                    />
-                </div>
-            </div>
             <AnimatePresence mode='wait' onExitComplete={closeHandler}>
                 {showDescription && (
                     <motion.div
@@ -81,19 +68,24 @@ const Modal: React.FC<ModalProps> = ({
                         })}
                     >
                         <div className={`w-full flex items-center justify-between lg:${orientation === 'right' ? 'flex-row' : 'flex-row-reverse'}`}>
-                            <div className={'flex flex-col'}>
-                                <h1 className={`text-secondary text-xl font-bold ${lato.className}`}> {workshop.title} </h1>
-                                <div className="flex items-center gap-3">
-                                    <span className="bg-gray-200 text-xs px-2 py-1 rounded">{workshop.date}</span>
-                                    <span className="bg-gray-200 text-xs px-2 py-1 rounded">{workshop.duration} hours</span>
-                                </div>
-                            </div>
+                            <h1 className={`text-secondary text-xl font-bold ${lato.className}`}> {workshop.title} </h1>
                             <IoMdCloseCircleOutline
                                 className="cursor-pointer"
                                 size={30}
                                 onClick={closeButtonClickHandler}
                             />
                         </div>
+                        <div className="flex items-center space-x-[8px]">
+                    <span className="bg-[#B0B0B033] text-secondary text-[10px] px-[10px] py-[5px] rounded-[98px]">
+                        {workshop.date}
+                    </span>
+                    <span className="bg-[#B0B0B033] text-secondary  text-[10px] px-[10px] py-[5px] rounded-[98px]">
+                        {workshop.duration} hours
+                    </span>
+                    <span className="bg-[#B0B0B033] text-secondary  text-[10px] px-[10px] py-[5px] rounded-[98px]">
+                        {workshop.speakerName}
+                    </span>
+                </div>
                         <div className={`py-[16px] text-xs sm:text-sm text-secondary ${lato.className} lg:${orientation === 'right' ? 'text-left' : 'text-right'}`}>
                             {workshop.description}
                         </div>
